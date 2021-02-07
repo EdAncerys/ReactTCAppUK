5d84b1a4e9e7ab52f63185759ecb0883f46c3331
Ed <lookatemail@gmail.com>
2021-02-07 11:44:45 +0000

    Add color pallete to the project

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import colors from '../config/colors';
+
 export default function NavigationBar({ props }) {
   return (
     <div style={styles.container}>
+      <div>Logo</div>
       <div>NavBar</div>
     </div>
   );
 }
 
 const styles = {
-  container: {},
+  container: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    display: 'grid',
+    alignContent: 'center',
+    gridTemplateColumns: 'auto auto',
+    height: '100px',
+    borderBottom: `1px solid ${colors.silver}`,
+    color: '#0275d8',
+    fontSize: '4vh',
+    userSelect: 'none',
+    cursor: 'default',
+    zIndex: 1,
+  },
 };
---
+++ src/config/colors.jsx
@@ -0,0 +1,7 @@
+export default {
+  black: '#292b2c',
+  silver: '#e8e9e9',
+  white: '#f7f7f7',
+  yellow: '#f0ad4e',
+  blue: '#0275d8',
+};
